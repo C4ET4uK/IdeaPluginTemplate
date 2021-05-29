800966b7d6e947a79e2b4d68a40dda730a28bb55
GitHub Action <action@github.com>
2021-05-29 07:31:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/c4et4uk/ideaplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.c4et4uk.ideaplugintemplate.services
+
+import com.github.c4et4uk.ideaplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
